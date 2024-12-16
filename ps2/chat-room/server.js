6e31e25148d4c4c7fe4ce448c1c49f06fb126226
vedha-varshini <152283972+vedha-varshini@users.noreply.github.com>
2024-12-16 09:54:40 +0530

    server.js

--- ps2/chat-room/server.js
+++ ps2/chat-room/server.js
@@ -7,19 +7,19 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
