40e48d2d10c734c44a34288f4105b91be8c7be28
vedha-varshini <152283972+vedha-varshini@users.noreply.github.com>
2024-12-16 10:03:13 +0530

    Add files via upload

---
+++ ps2/drawing-app/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("drawing", (data) => {
+    socket.broadcast.emit("drawing", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
